221f0121ef3354c49300f631fa778c679de4bad0
himanshii11 <himanshi.gupta@merapashu360.com>
2024-07-08 17:34:50 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,335 @@
+export const Bio = {
+  name: "Himanshi Gupta",
+  roles: [
+    "Software Developer",
+  ],
+  description:"I am a Software Development Engineer with a strong background in designing and developing scalable Microservices, Backends and Websites. Proficient in using various frameworks including Node.Js, Nest.Js, Next.Js and React.js. Skilled in multiple programming languages such as JavaScript, TypeScript, C++ and Python.",
+  github: "https://github.com/hiimanshii",
+  resume:
+    "https://drive.google.com/file/d/116eVlXOvv6dlsWbckhvBz-o08GUKME9p/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/himanshi-gupta-/",
+  phone:"8576960527",
+  email: "himanshigupta286@gmail.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:"https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png"
+            },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "NestJS",
+        image:
+          "https://www.vectorlogo.zone/logos/nestjs/nestjs-icon.svg",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Typescript",
+        image:"https://cdn.icon-icons.com/icons2/2415/PNG/512/typescript_original_logo_icon_146317.png"
+      },
+    ],
+  },
+  {
+    title: "Cloud",
+    skills: [
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firestore",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvCC9GOC_trNrnzh1ZYRv2KmaH8vpzdut2Sg&s",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Microsoft Clarity",
+        image:
+          "https://www.pitchbreak.us/content/images/2023/08/Tools---Clarity@10x.webp",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Heroku",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MPjYmDpC9Wii__RevOebbKtCtoYG3t6zzw&s",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "REST",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmJoxiAXVIxedd5WnxL3yepJpACK2lmCSl9w&s",
+      },
+      {
+        name: "Saas",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVA4Nj58BmB7Mvkcu6OGqGLDHKuA5vBEYfAw&s",
+      },
+      {
+        name:"C++",
+        image:"https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+      },
+      {
+        name:"MySQL",
+        image:"https://e7.pngegg.com/pngimages/747/798/png-clipart-mysql-logo-mysql-database-web-development-computer-software-dolphin-marine-mammal-animals-thumbnail.png",
+      },
+      {
+        name:"PHP",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0KV9Uvvg89ydvzlULlMUoccJ1ZynkM0SWEQ&s",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTalXgLrWhnc8M3WjPBmqiyxdML39yJYeSDYw&s",
+    role: "Software Developement Engineer",
+    company: "MeraPashu360",
+    date: "Oct 2023 - June 2024",
+    desc: "Contributed to the MeraPashu360 Website [https://merapashu360.com], implementing technologies like React, Redux, Saas, Node.js, GraphQL, Firebase, and Nest.js. Developed an internal POS solution, enhancing order accuracy by 20% and reducing processing time by 30%. Led the creation of a listing service with a Schedule Price Update feature, automating price modifications and cutting manual tasks by 50%. Implemented automated Postman testing for thorough API validation. Worked on integrating Zoho ERP to ERPNext, maintaining the ERPNext LMS, and provided training to the HR team. ",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Typescript",
+      "Tailwind",
+      "Saas",
+      "HTML",
+      "CSS",
+      "Graphql",
+      "REST",
+      "NextJS",
+      "NestJS",
+      "Firebase",
+      "Postman",
+      "Google Cloud",
+      "ERPnext"
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://miro.medium.com/v2/resize:fit:840/1*MBKK6GRWGbbjsO3Ca93Etg.jpeg",
+    role: "Fullstack Engineering Intern",
+    company: "The Sparks Foundation",
+    date: "May 2022 - June 2022",
+    desc:"Developed a responsive Basic Banking System web application, improving accessibility and usability across multiple devices.Developed the Web Application using a comprehensive tech stack including HTML, CSS, JavaScript, Bootstrap,PHP, and MySQL, ensuring robust functionality and seamless user experience. Reduced page load time by 40% through optimized code and efficient resource utilization.Deployed the website on Heroku, a container-based cloud platform, for rapid deployment and scaling.Achieved  99.9% uptime, ensuring continuous availability and minimizing disruptions for users.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstarp",
+      "Heroku",
+      "MySQL",
+      "PHP",
+    ],
+    doc: "",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZEKqkPhI9rAJoqS65UYKhx2ByjWPTVppzhFTHtOf4HnqKDVkFCd7A2H7qo4wPilRTnFQ&usqp=CAU",
+    role: "Campus Ambassador",
+    company: "DOBOT Company",
+    date: "May 2020 - June 2020",
+    desc: "Circulated filers through social media channels, resulting in a 30% increase in Dobot India's Instagram page reach within two weeks. Achieved a 20% boost in  follower engagement through targeted campaigns. To engage fellow students to volunteer for campus initiative events.",
+    doc: "",
+  },
+
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-KlAhg76i0cil5Pxd2ySwGb2LNL7nL9Cf3Q&s",
+    school: "Delhi Technological University, Delhi",
+    date: "Aug 2019 - May 2023",
+    grade: "8.66 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Delhi Technological University, Delhi. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaknnvtF0Pb-gYpshZeoeexI_DeuGsyiMWtA&s",
+    school: "Rani Laxmi Bai Memorial School, Lucknow",
+    date: "May 2018",
+    grade: "80.6%",
+    desc: "I completed my class 12 high school education at Rani Laxmi Bai Memorial School, Lucknow, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaknnvtF0Pb-gYpshZeoeexI_DeuGsyiMWtA&s",
+    school: "Rani Laxmi Bai Memorial School, Lucknow",
+    date: "May 2016",
+    grade: "93.6%",
+    desc: "I completed my class 10 education at Rani Laxmi Bai Memorial School, Lucknow, where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Banking System",
+    description:
+      "Developed a responsive Basic Banking System web application, improving accessibility and usability across multiple devices",
+    image:
+      "https://user-images.githubusercontent.com/67001353/102710492-bc08e200-42d8-11eb-8709-631c7f12afc2.png",
+    tags: [
+   
+    ],
+    category: "web app",
+    github: "https://github.com/hiimanshii/Banking-System",
+    webapp: "",
+  },
+  {
+    id: 9,
+    title: "Tile Based Game",
+    description:
+      "A 2-D scrolling platform game developed using Pygame that includes sprites, collisions, and powerups & more.This project incorporates the Tiled Map Editor and Pygame modules of Python.",
+    image:"https://www.pygame.org/thumb/fdc302f007b53c49904a524cda99774d.png",
+    tags: [
+     
+    ],
+    category: "game dev",
+    github: "https://github.com/hiimanshii/Tile-Based-Game-BOB-s-world",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "AI-Virtual Assistant",
+    description:"A Fully-featured AI assistant software capable of giving a completely hands-free experience for operating PC just by your voice. Includes multiple features such as searching the web, documentations, launching apps, music. Optimized the software for visually impaired and audience suffering from some mental illnesses, it's a boon for them.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1358/1*ql_JXpi66FBOrnPcmB8yQg.jpeg",
+    tags: [],
+    category: "ai",
+    github: "https://github.com/hiimanshii/AI-Virtual-Assistant",
+    webapp: "", 
+  },
+  {
+    id: 1,
+    title: "VoteBooth",
+    description:
+      "Developed a secure and transparent voting application utilizing NEAR blockchain technology",
+    image:
+      "https://rejolut.com/wp-content/uploads/2022/02/voting7.png",
+    tags: [
+    ],
+    category: "web app",
+    github: "https://github.com/hiimanshii/VoteBooth",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "Line Follower Robot using ARDUINO",
+    description:
+      "Designed A-Line follower robot. It is an automated self-driven vehicle that follows a specific path indicated by a line having some particular width.",
+    image:
+      "https://content.instructables.com/FFZ/OG6Q/JGGTIBUY/FFZOG6QJGGTIBUY.jpg?auto=webp",
+    tags: [],
+    category: "embedded Systems",
+    github: "https://github.com/hiimanshii/Line-Follower-Robot-using-ARDUINO",
+    webapp: "",
+  },
+];
+
